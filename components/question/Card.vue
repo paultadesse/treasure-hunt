0304b1afe5bf866deef28f1b9e9f9c66c4de3a36
Paulos Tadesse <paulosta952@gmail.com>
2023-11-06 17:17:58 +0300

    change hint icon to button

--- components/question/Card.vue
+++ components/question/Card.vue
@@ -3,23 +3,14 @@
     <div class="bg-slate-600 text-slate-400 p-8 rounded-lg">
       <div>
         <div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6 float-right cursor-pointer"
+          <button
+            class="uppercase tracking-wider bg-purple-500 text-white px-4 py-1 rounded-full text-xs float-right cursor-pointer"
             @click="exposeHint"
           >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-            />
-          </svg>
+            show hint
+          </button>
         </div>
-        <div>
+        <div class="max-w-xl">
           {{ content }}
         </div>
       </div>
